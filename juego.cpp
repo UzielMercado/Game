ae2611194b915a0da147628656eb416a6b4b4f7a
Uziel Mercado <dan642811@gmail.com>
2022-11-28 18:31:45 -0600

    added Y to map

--- juego.cpp
+++ juego.cpp
@@ -2,27 +2,48 @@
 
 using namespace std;
 
-void DrawMap(int HeroPos, char GameMap[5])
+void DrawMap(int HeroPosX, int HeroPosY, char GameMap[5][5])
 {
     for(int i = 0; i < 5; i = i + 1)
     {
-        if(i != HeroPos)
+        for(int p = 0; p < 5; p = p + 1)
         {
-            cout << GameMap[i];
+            if(i != HeroPosX)
+            {
+                cout << GameMap[p][i];
+            }
+            else
+            {
+                if(p != HeroPosY)
+                {
+                    cout << GameMap[p][i];
+                }
+                else
+                {
+                    cout << 'H';
+                }
+            }
         }
-        else
-        {
-            cout << 'H';
-        }
+        cout << endl;
     }
 }
 
 int main()
 {
-    int HeroPos = 1;
+    int HeroPosX = 1;
+    int HeroPosY = 1;
     bool IsGameOver = false;
     char Input = ' '; 
-    char GameMap[5] = {'1','1','1','1','1'};
+    char GameMap[5][5] = 
+    {
+        {'1','1','1','1','1'},
+        {'1','1','1','1','1'},
+        {'1','1','1','1','1'},
+        {'1','1','1','1','1'},
+        {'1','1','1','1','1'}
+    };
+
+    DrawMap(HeroPosX, HeroPosY, GameMap);
 
     while (IsGameOver == false)
     {
@@ -30,13 +51,13 @@ int main()
 
         if(Input == 'd')
         {
-            HeroPos = HeroPos + 1;
+            HeroPosX = HeroPosX + 1;
         }
         else if(Input == 'a')
         {
-            HeroPos = HeroPos + 1;HeroPos = HeroPos - 1;
+            HeroPosX = HeroPosX + 1; HeroPosX = HeroPosX - 1;
         }
-        DrawMap(HeroPos, GameMap);
+        DrawMap(HeroPosX, HeroPosY, GameMap);
     }
     return 0;
 }
